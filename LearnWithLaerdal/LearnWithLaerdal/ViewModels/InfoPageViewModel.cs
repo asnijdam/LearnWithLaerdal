75cf4163fbf04a3a6979bbe72645a91bdafd1849
Anne Nijdam <Anne.Nijdam@laerdal.com>
2022-07-19 15:23:40 +0200

    nye filer

---
+++ LearnWithLaerdal/LearnWithLaerdal/ViewModels/InfoPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace LearnWithLaerdal.ViewModels
+{
+    public class InfoPageViewModel : BaseViewModel
+    {
+        public InfoPageViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://laerdal.com/"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
